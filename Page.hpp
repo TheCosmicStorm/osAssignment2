15f0063f68f130ce183d3d739a81384e6206408e
Will <captainclueless63@gmail.com>
2019-10-10 17:01:59 +1030

    Added 2 files
    -page .hpp
    -pagereplacement.cpp

---
+++ Page.hpp
@@ -0,0 +1,34 @@
+#ifndef PAGE_H
+#define PAGE_H
+
+class Page {
+    int age; //time page has existed
+    int tlu; //time last used
+public:
+    Page() {
+        age = 0;
+        tlu = 0;
+    }
+
+    //USEFUL FUNCTIONS
+    //age
+    void increaseAge() {
+        age++;
+    }
+    void resetAge() {
+        age = 0;
+    }
+
+    //time last used
+    void setTLU(int newTime) {
+        tlu = newTime;
+    }
+    int getTLU() {
+        return tlu;
+    }
+
+
+    ~Page();
+};
+
+#endif
